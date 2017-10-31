4c3719493a9517643c3a8ce839c3fa8456659720
Poore <PLS7438@eagle.usaa.com>
2017-10-31 10:02:40 -0500

    Worked on video_list and video_list_item. Still working on video_list_item

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDLILQBwEf-OGEihvE9rvRdqzYLJxS4o8M';
 
+
+
 // Create a new component.  This component should
 // produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] } ;
+
+        YTSearch({key: API_KEY, term: 'ken ham'}, (videos) => {
+            //this.setState({ videos: videos});
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML
